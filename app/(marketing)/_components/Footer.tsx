8078ecefd542050598cffad4cfb7f009f5400533
rawasaditya <rawasaditya@gmail.com>
2023-12-09 17:47:50 +0530

    Feat: Marketing page created

---
+++ app/(marketing)/_components/Footer.tsx
@@ -0,0 +1,23 @@
+import React from "react";
+import Logo from "@/components/Logo.tsx";
+import { Button } from "@/components/ui/button";
+import Link from "next/Link";
+const Footer = () => {
+  return (
+    <div className="fixed bottom-0 w-full h-14 px-4 border-t flex items-center bg-slate-100">
+      <div className="md:max-w-screen-2xl mx-auto flex items-center w-full justify-between">
+        <Logo />
+        <div className="space-x-4 md:block md:w-auto flex items-center justify-between w-full">
+          <Button size="sm" variant="ghost">
+            Privacy Policy
+          </Button>
+          <Button size="sm" variant="ghost">
+            Terms of Services
+          </Button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Footer;
